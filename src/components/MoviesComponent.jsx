4def685a23b463caf5f60527b2cbf8dcfb41d5d3
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 12:16:53 +0530

    Code Optimization - Extracted the Movies Table

--- src/components/MoviesComponent.jsx
+++ src/components/MoviesComponent.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { getMovies } from '../services/fakeMovieService'
-import Like from './common/Like';
 import Pagination from './common/Pagination';
 import { paginate } from './utils/paginate';
 import ListGroup from './common/ListGroup';
 import { getGenres } from '../services/fakeGenreService';
+import MoviesTable from './MoviesTable';
 
 export default function MoviesComponent() {
 
@@ -59,46 +59,13 @@ export default function MoviesComponent() {
             </div>
             <div className="col">
                 <p>Showing {filteredMovies.length} movies in database</p>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            paginatedMovies.map(movie => (
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <Like liked={movie.liked}
-                                            onClick={() => handleLike(movie)} />
-                                    </td>
 
-                                    <td>
-                                        <button
-                                            className="btn btn-danger btn-sm m-2"
-                                            onClick={() => handleDelete(movie)}
-                                        >
-                                            Delete
-                                        </button>
+                <MoviesTable
+                    movies={paginatedMovies}
+                    onDelete={handleDelete}
+                    onLike={handleLike}
+                />
 
-                                    </td>
-                                </tr>
-                            ))
-                        }
-
-
-                    </tbody>
-                </table>
                 <Pagination
                     itemsCount={filteredMovies.length}
                     pageSize={pageSize}
---
+++ src/components/MoviesTable.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Like from './common/Like';
+
+export default function MoviesTable(props) {
+
+    const { movies, onDelete, onLike } = props;
+
+    return (
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like liked={movie.liked}
+                                    onClick={() => onLike(movie)} />
+                            </td>
+
+                            <td>
+                                <button
+                                    className="btn btn-danger btn-sm m-2"
+                                    onClick={() => onDelete(movie)}
+                                >
+                                    Delete
+                                </button>
+
+                            </td>
+                        </tr>
+                    ))
+                }
+
+
+            </tbody>
+        </table>
+    )
+}
