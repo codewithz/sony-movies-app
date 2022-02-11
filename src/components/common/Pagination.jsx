b6fa74e7eb327151b0559f034803face63018bc8
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 10:06:18 +0530

    Designed the working of Pagination Component

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import _ from 'lodash';
 
 export default function Pagination(props) {
+
+    const { itemsCount, pageSize } = props;
+
+    const pageCount = Math.ceil(itemsCount / pageSize);
+    console.log("PageCount: ", pageCount);
+
+    if (pageCount === 1) return null;
+
+    const pages = _.range(1, pageCount + 1);
+
+    //[1,....., pageCount].map()
     return (
         <nav>
             <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                {
+                    pages.map((page) => (
+                        <li key={page} class="page-item">
+                            <a class="page-link">{page}</a>
+                        </li>
+                    ))
+                }
+
             </ul>
         </nav>
     )
