40df6486b0987744ccf34a5ae27940b21d8c1ad4
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 16:51:11 +0530

    Added the Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import MoviesHome from './components/MoviesHome';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import React, { Fragment } from 'react';
+import MoviesComponent from './components/MoviesComponent';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <main className="container">
+          <Switch>
+            <Route path="/movies" component={MoviesComponent} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
+          </Switch>
 
-          <MoviesHome />
 
         </main>
       </BrowserRouter >
