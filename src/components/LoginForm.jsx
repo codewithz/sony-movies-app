d808ad754ce5824940d7af9280bb68921b4a7ed0
Zartab Nakhwa <zartab@codewithz.com>
2022-02-14 10:34:49 +0530

    Forms- Controlled Element - Specific

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginForm() {
+
+    const accountObject = { username: '', password: '' };
+    const [account, setAccount] = useState(accountObject);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //Call the server
+        console.log('Submitted');
+    }
+
+    const handleChange = (event) => {
+        // console.log("Handle Change", event)
+        const accountClone = { ...account };
+        accountClone.username = event.currentTarget.value;
+
+        setAccount(accountClone);
+    }
+
     return (
         <div>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" className="form-control" />
+                    <input id="username"
+                        type="text"
+                        className="form-control"
+                        value={account.username}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="text" className="form-control" />
+                    <input id="password"
+                        type="text"
+                        value={account.password}
+                        className="form-control"
+
+                    />
                 </div>
                 <button className="btn btn-warning btn-sm m-2">Login</button>
             </form>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,24 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Navbar() {
     return (
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-            <Link class="navbar-brand" to="/movies">Sony Movies</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <Link className="navbar-brand" to="/movies">Sony Movies</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
 
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" to="/">Movies</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" to="/customers">Customers</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" to="/login">Login</NavLink>
                     </li>
 
